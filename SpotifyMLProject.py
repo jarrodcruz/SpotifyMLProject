b20fa20b0d93af337f11ae7a01119e8f6a7f7a6c
jrodcodes <cruz.jarroddd@gmail.com>
2024-04-30 00:07:18 -0700

    began data wrangling

--- SpotifyMLProject.py
+++ SpotifyMLProject.py
@@ -4,3 +4,21 @@
 
 import numpy as np
 import pandas as pd
+
+raw_data = pd.read_csv('spotify-2023.csv',encoding='ISO-8859-1')
+
+raw_data.head()
+raw_data.info()
+
+na = raw_data.isna() 
+na_val_in_col = na.any()
+
+# iterate through columns to get frequency of missing values
+for col, na_present in na_val_in_col.items():
+    if na_present:
+        curr_freq = raw_data.isnull().sum()[col]
+        print(f"{curr_freq} missing values in column '{col}'.")
+
+raw_data = raw_data.dropna(subset=['key'])
+
+raw_data = raw_data.dropna(subset = ['in_shazam_charts'])
